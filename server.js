fafa4b6f6989f3ea0e7ab5cd5e689f9b84b5e42c
sai10d005 <saikiran.bimbadhar@gmail.com>
2016-09-22 22:33:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ function createTemplate(data){
 }
 
 app.get('/:articleName', function (req, res) {
+  var articleName=req.params.articleName;    
   res.send(createTemplate(articles[articleName]));
 });
 
