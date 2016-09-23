ba6fbf45a9be2ad2d8b3c4cd45ec452d53d30632
sai10d005 <saikiran.bimbadhar@gmail.com>
2016-09-23 16:47:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,9 +65,9 @@ function createTemplate(data){
                 ${heading};
             </body1>
             <br>
-            <color>
+            <color1>
                 ${date};
-            </color>
+            </color1>
             <br>
             <br>
              ${content};
